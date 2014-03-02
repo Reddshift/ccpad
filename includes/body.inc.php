06f1e21bcb204485720b7335987138608f5dee8f
Matthew Redd <mredd@deseretbook.com>
2014-03-01 17:19:15 -0700

    Initial commit

---
+++ includes/body.inc.php
@@ -0,0 +1,75 @@
+<?php
+function print_header($title) {
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<meta name="description" content="Find Painting Contractors.com contractors in Utah for home additions & remodeling projects for your bath rooms, kitchen, decks, fencing, drywall, plastering, electrical, flooring, landscaping, painting, roofing, plumbing, heating, windows & doors">
+<meta name="keywords" content="Utah, painting, contractor, professional, paint, contractor, contractors, house, painter, painters, wasatch, front, ut, residential, commercial, reliable, home improvement, remodeling, free quote, free estimate, commercial painting, residential painting, Textured Ceilings, Power Washing, Garage Floors, Stucco Repair, Plaster & Drywall Repair, Signs & Logos, Repaint Specialists, Custom Homes, Interior/Exterior, Apartments & Condo Complexes, Plaster & Drywall, Faux Finishes, Shopping Centers, Office Buildings, Hotels, Commercial Buildings, Warehouses, Institutional">
+
+<title>Crystal Clear Painting and Decorating Co,. Inc - Utah's Premier Painting Contractor - The Best Interior and Exterior Paint Contractor along the Wasatch Front, Utah. </title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/top_nav.css" rel="stylesheet" type="text/css">
+
+<script type="text/javascript" src="js/prototype.js"></script>
+<script type="text/javascript" src="js/scriptaculous.js?load=effects"></script>
+<script type="text/javascript" src="js/lightbox.js"></script>
+<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+
+</head>
+
+<body onload="MM_preloadImages('images/header-over.jpg','images/pavers_logo_top-over.jpg')">
+
+<!--begin header //-->
+
+<div class="basic_container" style="height: 150px;">	
+	<div class="basic_shadow">
+	  <div id="header">
+	    <div align="center"><img src="images/banner.jpg" width="798" height="150" border="0" /></div>
+	  </div>	
+  </div>
+</div>
+
+<div id="basic_container">	
+	<div class="basic_shadow">		
+		<div id="mainmenu">			
+			<ul class="prodrop4">
+	<li class="top"><a href="index.html" id="home" class="top_link home_page"><span>Home</span></a></li>
+	<li class="top"><a href="residential.html" class="top_link about_page"><span>Residential</span></a>
+	</li>
+	<li class="top"><a href="commercial.html" class="top_link contact_page"><span>Commercial</span></a></li>
+	<li class="top"><a href="about.html" class="top_link contact_page"><span>About Us</span></a></li>
+    <li class="top"><a href="testimonials.html" class="top_link contact_page"><span>Testimonials</span></a></li>
+    <li class="top"><a href="suppliers.html" class="top_link contact_page"><span>Suppliers</span></a></li>
+    <li class="top"><a href="free_quote.html" class="top_link contact_page"><span>Free Quote</span></a></li>
+	<li class="top"><a href="contact.html" class="top_link contact_page"><span>Contact Us</span></a></li>
+			</ul>				
+		<div class="clear">&nbsp;</div>	
+		</div>	
+	</div>
+</div>
+<!--end header //-->
+<? } ?>
+
+
+<?php
+function print_footer() {
+?>
+<!--begin footer //-->
+<div id="basic_container">	
+	<div id="footer">
+		<div id="copyright">&copy; 2007 Copyright Crystal Clear Painting and Decorating Co., Inc.<br />
+		  All rights reserved.      </div>	
+		<div id="footer_links">Email: <a href="mailto:sales@crystalclearpaintinganddecorating.com">sales@crystalclearpaintinganddecorating.com<br>
+		</a>		</div>	
+  </div>	
+</div>
+<!--end footer //-->
+
+</body>
+
+</html>
+<? } ?>
