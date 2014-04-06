fc3639979251e5af588d67870374998ae5cac6a3
Matt Redd on DigitalOcean CCPaD <matthew.redd@gmail.com>
2014-04-06 01:48:52 -0400

    better cropped and resolution for mobile logo. tweaked menu icon size for mobile.  Adjusted padding/heith for mobile logo.

--- includes/body.inc.php
+++ includes/body.inc.php
@@ -8,6 +8,7 @@ function print_header($title) {
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
   <meta name="description" content="Find Painting Contractors.com contractors in Utah for home additions & remodeling projects for your bath rooms, kitchen, decks, fencing, drywall, plastering, electrical, flooring, landscaping, painting, roofing, plumbing, heating, windows & doors">
   <meta name="keywords" content="Utah, painting, contractor, professional, paint, contractor, contractors, house, painter, painters, wasatch, front, ut, residential, commercial, reliable, home improvement, remodeling, free quote, free estimate, commercial painting, residential painting, Textured Ceilings, Power Washing, Garage Floors, Stucco Repair, Plaster & Drywall Repair, Signs & Logos, Repaint Specialists, Custom Homes, Interior/Exterior, Apartments & Condo Complexes, Plaster & Drywall, Faux Finishes, Shopping Centers, Office Buildings, Hotels, Commercial Buildings, Warehouses, Institutional">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
   <title>Crystal Clear Painting and Decorating Co,. Inc - Utah's Premier Painting Contractor - The Best Interior and Exterior Paint Contractor along the Wasatch Front, Utah. </title>
 
@@ -24,12 +25,12 @@ function print_header($title) {
 
 <!--begin header //-->
 
-<div class="basic_container" style="height: 150px;">	
+<div class="basic_container">	
 	<div class="basic_shadow">
 	  <div id="header">
       <div align="center">
         <img src="images/banner.jpg" id="header_img_regular" width="798" height="150" border="0" />
-        <img src="images/banner.jpg" id="header_img_mobile" width="320" height="150" border="0" />
+        <img src="images/logo_mobile.png" id="header_img_mobile" />
       </div>
 	  </div>	
   </div>
@@ -39,18 +40,30 @@ function print_header($title) {
 	<div class="basic_shadow">		
 		<div id="mainmenu">			
 			<ul class="prodrop4">
-	<li class="top"><a href="index.html" id="home" class="top_link home_page"><span>Home</span></a></li>
-	<li class="top"><a href="residential.html" class="top_link about_page"><span>Residential</span></a>
-	</li>
-	<li class="top"><a href="commercial.html" class="top_link contact_page"><span>Commercial</span></a></li>
-	<li class="top"><a href="about.html" class="top_link contact_page"><span>About Us</span></a></li>
-    <li class="top"><a href="testimonials.html" class="top_link contact_page"><span>Testimonials</span></a></li>
-    <li class="top"><a href="suppliers.html" class="top_link contact_page"><span>Suppliers</span></a></li>
-    <li class="top"><a href="free_quote.html" class="top_link contact_page"><span>Free Quote</span></a></li>
-	<li class="top"><a href="contact.html" class="top_link contact_page"><span>Contact Us</span></a></li>
+        <li class="top"><a href="index.html" id="home" class="top_link home_page"><span>Home</span></a></li>
+        <li class="top"><a href="residential.html" class="top_link about_page"><span>Residential</span></a></li>
+        <li class="top"><a href="commercial.html" class="top_link contact_page"><span>Commercial</span></a></li>
+        <li class="top"><a href="about.html" class="top_link contact_page"><span>About Us</span></a></li>
+        <li class="top"><a href="testimonials.html" class="top_link contact_page"><span>Testimonials</span></a></li>
+        <li class="top"><a href="suppliers.html" class="top_link contact_page"><span>Suppliers</span></a></li>
+        <li class="top"><a href="free_quote.html" class="top_link contact_page"><span>Free Quote</span></a></li>
+        <li class="top"><a href="contact.html" class="top_link contact_page"><span>Contact Us</span></a></li>
 			</ul>				
-		<div class="clear">&nbsp;</div>	
+		  <div class="clear">&nbsp;</div>	
 		</div>	
+    <div id="mobilemenu">
+      <a href="#menu" class="border-menu" onclick="$('ul#mobilemenu_items').toggle();"></a>&nbsp;&nbsp; Call: <a href="tel:801-968-8700">801-968-8700</a>
+    </div>
+    <ul id="mobilemenu_items">
+      <li><a href="index.html" id="home" class="top_link home_page"><span>Home</span></a></li>
+      <li><a href="residential.html" class="top_link about_page"><span>Residential</span></a></li>
+      <li><a href="commercial.html" class="top_link contact_page"><span>Commercial</span></a></li>
+      <li><a href="about.html" class="top_link contact_page"><span>About Us</span></a></li>
+      <li><a href="testimonials.html" class="top_link contact_page"><span>Testimonials</span></a></li>
+      <li><a href="suppliers.html" class="top_link contact_page"><span>Suppliers</span></a></li>
+      <li><a href="free_quote.html" class="top_link contact_page"><span>Free Quote</span></a></li>
+      <li><a href="contact.html" class="top_link contact_page"><span>Contact Us</span></a></li>
+    </ul>				
 	</div>
 </div>
 <!--end header //-->
